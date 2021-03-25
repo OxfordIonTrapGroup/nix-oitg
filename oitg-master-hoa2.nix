2ed6be11afcb01ef03ec15abe8067050008eb5ae
David Nadlinger <code@klickverbot.at>
2021-03-25 23:26:14 +0000

    oitg-master-hoa2: Temporarily disable Julia, as it takes forever to build (and 1.6 fails)

--- oitg-master-hoa2.nix
+++ oitg-master-hoa2.nix
@@ -4,5 +4,8 @@ let
     dependencies = import ./src/dependencies.nix { inherit pkgs; };
     julia_16 = pkgs.callPackage ./src/julia/1.6.nix { inherit (pkgs.darwin.apple_sdk.frameworks) CoreServices ApplicationServices; };
 in pkgs.mkShell {
-  buildInputs = [dependencies.artiq-master julia_16];
+    buildInputs = [
+        dependencies.artiq-master
+        # julia_16
+    ];
 }
