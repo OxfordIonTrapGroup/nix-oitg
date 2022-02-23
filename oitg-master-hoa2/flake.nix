cbc77e787702c3ce9674a4bd2c4eaabad8c6f10b
David Nadlinger <code@klickverbot.at>
2022-02-23 18:27:49 +0000

    Move oitg-master-hoa2 to Nix flakes
    Other targets (which are mostly in personal use by DPN)
    to follow.
    
    This required some changes to the upstream flake.nix. We'd like
    to be able to do without that diff, but in the meantime, this is
    tracked automatically by the branch specified in flake.nix and
    the corresponding commit information in flake.lock.

---
+++ oitg-master-hoa2/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Environment for running ARTIQ master in lab one/HOA2";
+
+  inputs.artiq = {
+    url =
+      "git+ssh://gitlab.physics.ox.ac.uk/ion-trap/artiq.git?ref=dpn/nix-riscv";
+  };
+
+  outputs = { self, artiq }:
+    let pkgs = artiq.pkgs;
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        name = "artiq-dev-shell";
+        buildInputs = [
+          (pkgs.python3.withPackages (ps:
+            (with ps; [
+              aiohttp
+              dateutil
+              h5py
+              #numba
+              jsonschema
+              numpy
+              paramiko
+              prettytable
+              python-Levenshtein
+              scipy
+              pyserial
+              pyzmq
+            ]) ++ (with artiq.packages.x86_64-linux; [
+              llvmlite-new
+              migen
+              misoc
+              qasync
+              microscope
+            ])))
+          pkgs.llvmPackages_11.clang-unwrapped
+          pkgs.llvm_11
+          pkgs.lld_11
+          artiq.packages.x86_64-linux.openocd-bscanspi
+        ];
+      };
+    };
+
+  nixConfig = {
+    extra-trusted-public-keys =
+      "nixbld.m-labs.hk-1:5aSRVA5b320xbNvu30tqxVPXpld73bhtOeH6uAjRyHc=";
+    extra-substituters = "https://nixbld.m-labs.hk";
+    extra-sandbox-paths = "/opt";
+  };
+}
