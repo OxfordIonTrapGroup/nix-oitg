2f9477ea232a0fc0268b7db6d59351a733455bb3
David Nadlinger <code@klickverbot.at>
2022-11-13 20:40:24 +0000

    nix flake update; remove now-unnecessary unstable nixpkgs
    Might actually want to put that back and use Julia 1.8, though.

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     artiq.url = "git+ssh://git@gitlab.physics.ox.ac.uk/ion-trap/artiq.git";
 
-    # Julia 1.7 is not available from nixpkgs 21.11; this second copy can be removed
-    # once it is.
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
-
     # Oxford-flavoured ARTIQ packages. We pull them in as flake inputs so we can
     # conveniently update them using `nix lock`, etc., rather than manually having to
     # track hashes.
@@ -36,8 +32,8 @@
       flake = false;
     };
   };
-  outputs = { self, artiq, nixpkgs-unstable, src-andorEmccd, src-llama
-    , src-ndscan, src-oitg, src-oxart, src-oxart-devices }:
+  outputs = { self, artiq, src-andorEmccd, src-llama, src-ndscan, src-oitg
+    , src-oxart, src-oxart-devices }:
     let
       nixpkgs = artiq.nixpkgs;
       sipyco = artiq.inputs.sipyco;
@@ -117,7 +113,7 @@
         buildInputs = [
           python-env
           artiq.packages.x86_64-linux.openocd-bscanspi
-          nixpkgs-unstable.legacyPackages.x86_64-linux.julia_17-bin
+          nixpkgs.julia_17-bin
           nixpkgs.lld_11
           nixpkgs.llvm_11
         ];
